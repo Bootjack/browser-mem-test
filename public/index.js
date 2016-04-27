b71fc63e76d6614f66651be78ae811786a844504
Jason Hinebaugh <jason.hinebaugh@uspto.gov>
2016-04-27 13:50:42 -0400

    Keep global reference to child window in parent window

--- public/index.js
+++ public/index.js
@@ -1,10 +1,14 @@
 var button = document.getElementById('open-new-window-button'),
+    evilWindow,
     worker = new SharedWorker('worker.js');
 
 button.addEventListener('click', function () {
-    window.open('test.html', 'test');
+    evilWindow = window.open('test.html', 'test');
 });
 
 worker.port.onmessage = function(e) {
+    evilWindow.setTimeout(function () {
+        evilWindow.console.log('Mwuhahahaha!');
+    }, 500);
     console.log('From shared worker: ', e.data);
 }
